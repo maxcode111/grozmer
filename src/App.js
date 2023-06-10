f807b552dbb092f1b16cd90c74fc54be7222e945
maxcode111 <magomed.isaev.98@list.ru>
2023-06-10 17:36:43 +0300

    asd

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MainPage from "./components/MainPage/MainPage";
+
+function App() {
+  return (
+    <div>
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./MainPage.css";
+
+export default function MainPage() {
+  return (
+      <div>
+        <div className="icon-block"><img src="./img/logo.png" alt="logo" className='logo-pic'/> <p className='mer-title'>МЭРИЯ ГОРОДА <br/> ГРОЗНЫЙ</p></div>
+    </div>
+  )
+}
